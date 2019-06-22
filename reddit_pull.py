2514ac606e58a35006f4c0a1d7843628431d2782
Chris White <50224597+cawhitecode@users.noreply.github.com>
2019-06-21 21:19:19 -0500

    Updated to include time and rank
    Added time and rank. Time is unique

--- reddit_pull.py
+++ reddit_pull.py
@@ -2,27 +2,31 @@ import praw
 import psycopg2
 import schedule
 import time
+from datetime import datetime
 
 
-reddit = praw.Reddit(client_id='xxxxxxxxx',
-                     client_secret='xxxxxxxxxxxxxx',
-                     user_agent='xxxxxxxxxxxxx')
+reddit = praw.Reddit(client_id='xxxxxxxxxxxxx', client_secret='xxxxxxxxxxxxxxx', user_agent='my user agent')
 
 #object to index the subreddit info and to recall later for SQL translation
 class Subreddit(object):
-    def __init__(self, title, subreddit, score, numb_comments, subscribers):
+    def __init__(self, title, subreddit, score, numb_comments, subscribers, rank, time):
         self.title = title
         self.subreddit = subreddit
         self.score = score
         self.numb_comments = numb_comments
         self.subscribers = subscribers
+        self.rank = rank
+        self.time = time
 
-
+#stores objects for indexing later
 subreddit_info = []
+
 #using reddit API to make an object or submission and indexing them
 def reddit_pull():
     print('Reddit')
+    rank = 0
     for submission in reddit.subreddit('all').hot(limit=25):
+        rank +=1
         bad_title = str(submission.title)
         title = bad_title.replace("'", "%")
         title = (title[:200] + '..') if len(title) > 75 else title
@@ -30,19 +34,20 @@ def reddit_pull():
         score = int(submission.score)
         numb_comments = int(submission.num_comments)
         subscribers = int(submission.subreddit.subscribers)
-        subreddit_info.append(Subreddit(title, subreddit, score, numb_comments, subscribers))
+        time = datetime.today()
+        subreddit_info.append(Subreddit(title, subreddit, score, numb_comments, subscribers, rank, time))
 
 #this connects to the server and put the reddit info into a readable formart for sql
 def sql_inject():
-    connection = psycopg2.connect(user = "xxxxxxxxx", password = "xxxxxxxx", host = "xxxxxxxxxxxxx", port = "xxxxxxxxx", database = "postgres")
+    connection = psycopg2.connect(user = "xxxxxxxxxxxxxxxxx", password = "xxxxxxxxx", host = "xxxxxxxxxxx", port = "xxxxxxx", database = "postgres")
     cursor = connection.cursor()
     print('Connected!')
-    
+
 #this is where the magic happens for SQL convert
     for obj in subreddit_info:
-        format_sql  = """INSERT INTO reddit_info (title, subreddit, upvotes, comments, subscribers)
-        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}');"""
-        cursor.execute(format_sql.format(obj.title, obj.subreddit, obj.score, obj.numb_comments, obj.subscribers))
+        format_sql  = """INSERT INTO reddit_info_time_rank (title, subreddit, upvotes, comments, subscribers, time, rank)
+        VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}');"""
+        cursor.execute(format_sql.format(obj.title, obj.subreddit, obj.score, obj.numb_comments, obj.subscribers, obj.time, obj.rank))
 
     connection.commit()
     print("----SUCCESS----")
@@ -57,6 +62,7 @@ def reddit_all():
     print('cleared subreddit_info')
 
 #runs program every two hours or reddit_all to be more specific
+reddit_all()
 schedule.every(2).hours.do(reddit_all)
 
 while 1:
